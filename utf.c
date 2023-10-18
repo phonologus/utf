7d0d416df1f9f1e8445f824650785f69f908bd25
sean <phonologus@gmail.com>
2023-10-18 08:28:56 +0100

    Update utf.c
    Correct sate in copyright line

--- utf.c
+++ utf.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003 Sean Jensen
+ * Copyright (c) 2023 Sean Jensen
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
